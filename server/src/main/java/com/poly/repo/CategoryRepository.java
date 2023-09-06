8b87c24302e0363e3d1aa0dcb37686b5e8cf8ca2
D u o n g <96007994+Dyong46@users.noreply.github.com>
2023-09-06 13:48:46 +0700

    add Repository to address and category

--- server/src/main/java/com/poly/repo/CategoryRepository.java
+++ server/src/main/java/com/poly/repo/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.poly.repo;
 
 import com.poly.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 }
