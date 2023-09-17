4e455d9c1b3d192d16461db43ce589638315cca9
tuankietha <106296783+tuankietha@users.noreply.github.com>
2023-09-18 05:06:38 +0700

    fix header layout

--- client/src/components/Header/HeaderOrder.jsx
+++ client/src/components/Header/HeaderOrder.jsx
@@ -29,11 +29,44 @@ let datasRight = [
 
 ];
 
+let datasLeft = [
+  'Thông báo',
+  'Kênh người bán',
+  'Tải ứng dụng',
+  'Kết nối'
+]
+
 const HeaderOrder = () => {
   return (
     <div className="bg-[linear-gradient(-180deg,#f53d2d,#f63)]  pt-2 text-white">
       <div className="container">
-        <div className="flex justify-end h-2">
+        <div className="flex justify-between h-2">
+          <div className="flex justify-center align-center">
+            {datasLeft.map((item, index) => (
+              <button className="px-3 flex align-center text-xs text-left hover:text-orange" key={index} onClick={() => { }}>
+                {item}
+              </button>
+
+            ))}
+
+            <div className="py-1 w-5 h-5 justify-center block bg-white rounded-full">
+              <svg className="w-[12px] h-[12px] text-gray-800 dark:text-orange" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 2 19">
+                <path fillRule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clipRule="evenodd" />
+              </svg>
+            </div>
+
+            <div className="py-1 px-1 mx-1 w-5 h-5 justify-center block bg-white rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-[11px] w-[11px] dark:text-orange"
+                fill="currentColor"
+                viewBox="0 0 24 24">
+                <path
+                  d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+              </svg>
+            </div>
+          </div>
+
           <div className="flex justify-center align-center my-1">
             <div className="flex justify-center align-center ">
               {datasRight.map((item, index) => (
